8c56da059c8138c052c96f761cc8e3e3e137e33d
eugene-liyai <keugeneliyai@yahoo.com>
2017-01-17 21:15:01 +0300

    Modification of the fizzy_buzz method - addition of comments, implementation of car_class_lab assignment and testing

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,9 +1,12 @@
 def fizz_buzz(val):
 
+	# check fizzbuzz for specific inputs 
 	if val%5 == 0 and val%3 == 0:
 		return 'FizzBuzz'
+	# check if divisible by three
 	elif val%3 == 0:
 		return 'Fizz'
+	# check if divisible by five
 	elif val%5 == 0:
 		return 'Buzz'
 	else:
