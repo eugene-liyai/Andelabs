ba8e0887e7f05da5395363f636d3334c322c5342
eugene-liyai <keugeneliyai@yahoo.com>
2017-01-18 10:11:07 +0300

    complete word_count implementation, previous code failed in some test. Final code passed all tests

--- word_count.py
+++ word_count.py
@@ -1,25 +1,25 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-def word(sentence):
-	
-	word_sent = word_tokenize(sentence)
-	# words = sentence.split('')
-
-	# set for tracking
-	seen = set()
-	output = {}
-
-	for word in word_sent:
-		if word not in seen:
-			seen.add(word)
-			output[word] = 1
-		else:
-			output[word] += 1
-
-	return output
-
-	
+def words(string_of_words):
+    word_sent = string_of_words.split()
+    
+    # set for tracking
+    seen = set()
+    output = {}
+    
+    for word in word_sent:
+        if word not in seen:
+            seen.add(word)
+            if word.isdigit() is True:
+              output[int(word)] = 1
+            else:
+              output[word] = 1
+        else:
+            if word.isdigit() is True:
+              output[int(word)] += 1
+            else:
+              output[word] += 1
+    
+    return output
 
 
 if __name__ == '__main__':
-	print(word('GO    go   go 1 2 3 1'))
+	print(words('testing 1 2 testing'))
