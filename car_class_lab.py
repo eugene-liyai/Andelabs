f320c63fb5d65d45e82758ad6e3488ccadd22dbc
eugene-liyai <keugeneliyai@yahoo.com>
2017-01-18 09:07:49 +0300

    correction on car class

--- car_class_lab.py
+++ car_class_lab.py
@@ -1,21 +1,13 @@
 class Car(object):
     
     # initializing car defaults
-    def __init__(self, name=None, model=None, _type=None):
+    def __init__(self, name='General', model='GM', _type=None):
         # default speed of a parked car
+        self.name = name
+        self.model = model
         self.speed = 0
-
-        # setting default name and model
-        if name is None:
-            self.name = 'General'
-            self.num_of_doors = 4
-        else:
-            self.name = name
-
-        if model is None:
-            self.model = 'GM'
-        else:
-            self.model = model
+        self.num_of_doors = 4
+        self.model = model
 
         # A saloon is the default car type
         if _type is None:
@@ -32,10 +24,8 @@ class Car(object):
         # Porsche & Koennigsegg = 2 doors, default = 4 doors
         if name is not None and (name is 'Porshe' or name is 'Koenigsegg'):
             self.num_of_doors = 2
-        elif name is not None and (name is not 'Porshe' or name is not 'Koenigsegg'):
-            self.num_of_doors = 4
         else:
-            pass
+            self.num_of_doors = 4
 
     # check if car is saloon
     def is_saloon(self):
@@ -48,7 +38,7 @@ class Car(object):
     def drive(self, move):
         assert isinstance(move, int), "drive argument has to be an integer"
         if self._type == 'trailer':
-            self.speed = move * 11
+            self.speed = 77
             return self
         elif self._type == 'saloon':
             self.speed = 10 ** move
--- word_count.py
+++ word_count.py
@@ -1,2 +1,25 @@
-def word():
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+def word(sentence):
 	
+	word_sent = word_tokenize(sentence)
+	# words = sentence.split('')
+
+	# set for tracking
+	seen = set()
+	output = {}
+
+	for word in word_sent:
+		if word not in seen:
+			seen.add(word)
+			output[word] = 1
+		else:
+			output[word] += 1
+
+	return output
+
+	
+
+
+if __name__ == '__main__':
+	print(word('GO    go   go 1 2 3 1'))
